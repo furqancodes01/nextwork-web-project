25f929e953d81dcad03a78aba6868e3614e6ce7c
MOHAMMED FURQANUDDIN <mail2furqan01@gmail.com>
2026-01-23 01:05:34 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -9,4 +9,8 @@ if systemctl is-active --quiet tomcat; then
   sudo systemctl stop tomcat
 fi
 
+# Remove old deployment
+sudo rm -rf /usr/share/tomcat/webapps/nextwork-web-project
+sudo rm -rf /usr/share/tomcat/webapps/nextwork-web-project.war
+
 exit 0
