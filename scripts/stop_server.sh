832a68e47d4271a9205d779263fbb2630ea298b8
MOHAMMED FURQANUDDIN <mail2furqan01@gmail.com>
2026-01-23 00:24:36 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+# Stop Apache if running
 if systemctl is-active --quiet httpd; then
   sudo systemctl stop httpd
 fi
 
-if pgrep -f tomcat > /dev/null; then
-  sudo /opt/tomcat/bin/shutdown.sh
+# Stop Tomcat if running (using systemctl for yum-installed tomcat)
+if systemctl is-active --quiet tomcat; then
+  sudo systemctl stop tomcat
 fi
+
+exit 0
