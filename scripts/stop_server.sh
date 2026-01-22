149266ac61f66e699ad498ddaf2b30465e54a202
MOHAMMED FURQANUDDIN <mail2furqan01@gmail.com>
2026-01-23 01:36:34 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -4,14 +4,15 @@ if systemctl is-active --quiet httpd; then
   sudo systemctl stop httpd
 fi
 
-# Stop Tomcat if running (using systemctl for yum-installed tomcat)
+# Stop Tomcat if running
 if systemctl is-active --quiet tomcat; then
   sudo systemctl stop tomcat
+  # Wait for Tomcat to fully stop
+  sleep 5
 fi
 
-# Remove old deployment
+# Remove old deployment - do this AFTER Tomcat stops
 sudo rm -rf /usr/share/tomcat/webapps/nextwork-web-project
 sudo rm -rf /usr/share/tomcat/webapps/nextwork-web-project.war
 
 exit 0
-
